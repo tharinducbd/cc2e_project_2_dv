c58e172a8bcbc83322d17608d07310afd3d07774
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-05 16:42:35 +0530

    plot correction: input values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -11,6 +12,6 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Squre of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', labelsize=10)
 
 plt.show()
