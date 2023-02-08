dd796fd4396ebef4294087720405afad2d75aaea
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-08 15:41:05 +0530

    change plot color

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,7 +6,7 @@ y_values = [x**2 for x in x_values]
 plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
