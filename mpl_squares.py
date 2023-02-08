7b26c7fff3ce24e26f992f53b5fff91c4c6db01a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-08 06:00:00 +0530

    Reverted to fig, ax as it is the convention.

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,15 +6,15 @@ squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 # Using a built-in style
 plt.style.use('seaborn-v0_8')
 
-figure, axes = plt.subplots()
-axes.plot(input_values, squares, linewidth=3)
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
-axes.set_title("Square Numbers", fontsize=24)
-axes.set_xlabel("Value", fontsize=14)
-axes.set_ylabel("Squre of Value", fontsize=14)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Squre of Value", fontsize=14)
 
 # Set size of tick labels.
-axes.tick_params(axis='both', labelsize=10)
+ax.tick_params(axis='both', labelsize=10)
 
 plt.show()
