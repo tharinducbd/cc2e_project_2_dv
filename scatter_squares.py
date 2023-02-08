eb24273b84b046940d8e2caf62edf4360e52bc9c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-08 16:03:03 +0530

    save to file

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,7 +6,7 @@ y_values = [x**2 for x in x_values]
 plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -19,4 +19,8 @@ ax.tick_params(axis='both', which='major', labelsize=10)
 # Set the range for each axis.
 ax.axis([0,1100, 0, 1100000])
 
+# To save the plot instead of rendering.
+# plt.savefig('squares_plot.png', bbox_inches='tight')
+
+# Render the plot.
 plt.show()
