7c240f45f4cc52ea0343e0e627c9937be118fe26
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-08 06:57:16 +0530

    scatter v1 styling

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=12)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick lables.
+ax.tick_params(axis='both', which='major', labelsize=10)
 
 plt.show()
