8ffefd90d9b7ad607ce1d19a2122740df811ec8a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-10 06:08:11 +0530

    randomw_walk.py v1

--- random_walk.py
+++ random_walk.py
@@ -14,4 +14,26 @@ class RandomWalk:
 
     def fill_walk(self):
         """Calculate all the points in the walk."""
-        
+
+        # Keep taking steps until the walk reaches the desired length.
+        while len(self.x_values) < self.num_points:
+
+            # Decide which direction to go and how far to go that way.
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # Reject moves that go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calculate the new position.
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
