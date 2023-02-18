9c8c2b5271a3b6b11615ee7e63c153a2c19590dd
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-18 11:07:03 +0530

    enumerate()

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,4 +4,6 @@ filename = 'cc2e_codes/project_2/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
