216a6b1af1012bedb215c5bf0cb26d95f1f16e98
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-18 11:39:28 +0530

    highs plotted

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'cc2e_codes/project_2/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -11,6 +13,17 @@ with open(filename) as f:
 
     # Get high temperatures from the file.
     highs = [int(row[5]) for row in reader]
+    print(highs)
 
-print(highs)
+# Plot the high temperaturs.
+plt.style.use('dark_background')
+fig, ax = plt.subplots(figsize=(10,6))
+ax.plot(highs, c='red')
 
+# Format plot.
+ax.set_title("Daily high temps, July 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
