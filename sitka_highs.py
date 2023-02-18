ecb3549db1631b145d6931d03625ba169e8309ce
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-18 11:28:14 +0530

    highs recorded

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,6 +4,13 @@ filename = 'cc2e_codes/project_2/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
+    print(header_row)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # Get high temperatures from the file.
+    highs = [int(row[5]) for row in reader]
+
+print(highs)
+
