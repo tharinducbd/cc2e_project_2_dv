d1cb4a1855d51e15d9115ba68693ceb2e4317f7f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-22 06:51:03 +0530

    Two series plot

--- sitka_highs_3.py
+++ sitka_highs_3.py
@@ -9,20 +9,23 @@ with open(filename) as f:
     header_row = next(reader)
 
     # Get dates and hight temps from this file.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperaturs.
+# Plot the high and low temperaturs.
 plt.style.use('dark_background')
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='yellow')
 
 # Format plot.
-ax.set_title("Daily high temps, 2018", fontsize=24)
+ax.set_title("Daily high & low temps - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
