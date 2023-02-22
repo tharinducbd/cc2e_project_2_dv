c3a81905bc9e216c4632e31f939426322fcad47d
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-22 14:26:37 +0530

    try-except added. code complete.

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -8,15 +8,22 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
     # Get dates and hight temps from this file.
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high and low temperaturs.
 plt.style.use('dark_background')
@@ -26,7 +33,7 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='yellow', alpha=0.2)
 
 # Format plot.
-ax.set_title("Daily high & low temps - 2018", fontsize=24)
+ax.set_title("Daily high & low temps - 2018\nDeath Valley, CA", fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
