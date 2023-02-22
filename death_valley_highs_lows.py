084b78c401c5052dfcfba105b575f3f3e43d05cf
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-22 13:57:00 +0530

    death valley data: attempt 1

---
+++ death_valley_highs_lows.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+filename = 'cc2e_codes/project_2/data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates and hight temps from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# Plot the high and low temperaturs.
+plt.style.use('dark_background')
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='yellow', alpha=0.2)
+
+# Format plot.
+ax.set_title("Daily high & low temps - 2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
