9d4e5f2f9deae8bf13ded43b9e6c636e277029bb
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-25 17:07:06 +0530

    mags, lons, lats extracted

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,6 +5,23 @@ filename = 'cc2e_codes/project_2/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'cc2e_codes/project_2/data/eq_readable_1d_m1.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+# # Make readable output (temporary) to see the data structure
+# readable_file = 'cc2e_codes/project_2/data/eq_readable_1d_m1.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
