c3c8b4ed405cf60a52efc9edc07c8e1ce4f7c0de
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-03-04 05:14:07 +0530

    Complete V1

--- visual_repos_sql.py
+++ visual_repos_sql.py
@@ -16,3 +16,22 @@ response_dict = r.json()
 repo_dicts = response_dict['items']
 repo_names, stars = [], []
 
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars, 
+    }]
+
+my_layout = {
+    'title': 'Most-starred SQL repositories on GitHub',
+    'xaxis': {'title': 'Repository',},
+    'yaxis': {'title': 'Stars',}
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='sql_repos.html')
