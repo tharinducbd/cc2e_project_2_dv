4a81f08f3757b128ba84429be7cfc375e3b8e569
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-03-04 05:49:10 +0530

    Bar style modified

--- visual_repos_python.py
+++ visual_repos_python.py
@@ -25,6 +25,11 @@ data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+        },
+    'opacity': 0.6,
     }]
 
 my_layout = {
