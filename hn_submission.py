3f3d1f52ee28e3fca2f5d30dd76794db625c324f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-03-06 19:14:21 +0530

    Status Code 200

---
+++ hn_submission.py
@@ -0,0 +1,9 @@
+from operator import itemgetter
+
+import requests
+
+# Make an API call and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
