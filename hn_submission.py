4bc44a7efdb62803f444ba547cc76592563621a9
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-03-11 05:59:01 +0530

    Added dict.setdefault - my own!!!

--- hn_submission.py
+++ hn_submission.py
@@ -12,6 +12,7 @@ submission_ids = r.json()
 submission_dicts = []
 
 for submission_id in submission_ids[:30]:
+
     # Make a separate API call for each submission.
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
@@ -19,19 +20,18 @@ for submission_id in submission_ids[:30]:
     response_dict = r.json()
 
     # Build a dictionary for each article.
-    if 'descendants' in response_dict:
-        submission_dict = {
-            'title': response_dict['title'],
-            'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
-            'comments': response_dict['descendants'],
-            }
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict.setdefault('descendants', 0),
+        }
+
     submission_dicts.append(submission_dict)
 
-submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), 
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                           reverse=True)
 
 for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
-# temp
